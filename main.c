e6226c616c23bcc636cfa99707e562fb3c72d5af
nathanB527 <nbledsoe002@gmail.com>
2026-02-26 13:51:58 -0600

    Refactored to use a struct for builtin commands, allows for better expandability in the future.

--- main.c
+++ main.c
@@ -5,8 +5,10 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <limits.h>
+
+//HELPER FUNCTIONS
+
 char *find_command(char *command, char *path) {
-    printf("path is %s\n", path);
     for (char *dir = strtok(path, ":"); dir != NULL;
         dir = strtok(NULL, ":")) {
             char fullpath[1024];
@@ -19,6 +21,52 @@ char *find_command(char *command, char *path) {
     free(path);
     return NULL;
 }
+
+//BUILTIN FUNCTIONS
+void builtin_exit(char **args) {
+    exit(0);
+}
+
+void builtin_echo(char **args) {
+    for (int j = 1; args[j] != NULL; j++) {
+        printf("%s ", args[j]);
+    }
+    printf("\n");
+}
+void builtin_type(char **args) {
+    char *exec_path = find_command(args[0], strdup(getenv("PATH")));
+    if (exec_path != NULL) {
+        printf("%s is %s\n", args[0], exec_path);
+    }
+    else {
+        printf("%s: not found\n", args[1]);
+    }
+}
+
+void builtin_pwd(char **args) {
+    char cwd[PATH_MAX];
+    printf("%s\n", getcwd(cwd, sizeof(cwd)));
+}
+
+void builtin_cd(char **args) {
+    if(chdir(args[1]) != 0) {
+        printf("cd: %s: No such file or directory\n", args[1]);
+    }
+}
+//Defining struct, to store a name and a function
+typedef struct {
+    const char *name;
+    void (*func)(char **args);
+} Builtin;
+//Table mappings to the functions
+Builtin builtins[] = {
+    {"exit", builtin_exit},
+    {"echo", builtin_echo},
+    {"type", builtin_type},
+    {"pwd", builtin_pwd},
+    {"cd", builtin_cd},
+    {NULL, NULL}
+};
 
 int main(int argc, char *argv[]) {
   // Flush after every printf
@@ -53,29 +101,32 @@ int main(int argc, char *argv[]) {
       }
       args[i+1] = NULL;
 
+    // Run cd
     //Run builtin command
-    char builtin_path[256] = "/home/nathan/Projects/splatsh/builtins/bin";
-    char *exec_path = find_command(args[0], strdup(builtin_path));
-    if (exec_path != NULL) {
+    int found = 0;
+    for (int i = 0; builtins[i].name; i++) {
+        if (!strcmp(args[0], builtins[i].name)) {
+            builtins[i].func(args);
+            found = 1;
+            break;
+        }
     }
-    else {
-    exec_path = find_command(args[0], strdup(getenv("PATH")));
-    }
-
-
-    //Run external command
-    //Use fork to create a child process before calling execv
-    if (exec_path != NULL) {
-        pid_t pid = fork();
-        if (pid == 0 ) {
-            execv(exec_path, args);
+    if (!found){
+        char *exec_path = find_command(args[0], strdup(getenv("PATH")));
+        if (exec_path != NULL) {
+            pid_t pid = fork();
+            if (pid == 0 ) {
+                execv(exec_path, args);
+            }
+            else {
+                wait(NULL);
+            }
         }
         else {
-            wait(NULL);
-        }}
-    else {
-        printf("%s: not found\n", input);
+            printf("%s: not found\n", input);
+        }
     }
+
   }
   return 0;
 }
